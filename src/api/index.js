0464e8e17e353e00b0413d9675ff18d8d819b0ec
FarrukhS <909tjk@gmail.com>
2022-09-19 11:12:39 -0500

    updated api link

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000' });
+const API = axios.create({ baseURL: 'https://capture-memory.herokuapp.com/posts' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
